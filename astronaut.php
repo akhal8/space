9d9e2b617a2f4d5ccc04700aa6fe6882c369c5a0
akhal8 <alikhalid1024@gmail.com>
2021-12-09 22:28:57 +0000

    update

--- astronaut.php
+++ astronaut.php
@@ -56,10 +56,8 @@
         Placeholder usedd to give user hint on what to put in the textbox.-->
         <div class="col-md-12 justify-content-center text-center p-2">
         <form action="ast_adding.php" method="POST">
-        <div class= "p-1">Astronaut name</div> <input type="text" name="last_name" placeholder="James" value="<?php echo htmlspecialchars($email) ?>"><br>
-        <div class="red-text"><?php echo $errors['last_name']; ?></div>
-        <div class="p-1"> Number of Missions</div> <input type="text" name="no_missions" placeholder="10,11,20..." value="<?php echo htmlspecialchars($title) ?>" ><br>
-        <div class="red-text"><?php echo $errors['o_missions']; ?></div>
+        <div class= "p-1">Astronaut name</div> <input type="text" name="last_name" placeholder="James"><br>
+        <div class="p-1"> Number of Missions</div> <input type="text" name="no_missions" placeholder="10,11,20..."><br>
         <div class= "p-1"><input type="submit" name="ast_submit"></div>
         </form>
         <a div class="col-md-12 justify-content-center text-center p-2" href='index.php'><button>Main Page</button></a>
