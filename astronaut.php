7096f0e97f89dacf1dca8dbb469901aceb615e7b
akhal8 <alikhalid1024@gmail.com>
2021-12-09 21:16:21 +0000

    update

--- astronaut.php
+++ astronaut.php
@@ -8,7 +8,31 @@
     </head>
     <body>
       <?php
-      include 'validation2.php'; 
+      $ast_name = $ast_no_missions = "";
+      $ast_errors = array('ast_name' => '','ast_no_missions' =>'');
+      if (isset($_POST['ast_submit'])) {
+        
+      
+          if (empty($_POST["ast_name"])) {  
+              $ast_errors['ast_name'] = 'An Input of characters with our without whitespaces is required';  
+        } else {  
+             $ast_name = ($_POST["ast_name"]);  
+          //         //check if name only contains letters and whitespace  
+                 if (!preg_match("/^[a-zA-Z ]*$/",$ast_name)) {  
+                  $ast_errors['ast_name'] = 'An Input of characters with our without whitespaces is required';
+               }
+             }
+          if (empty($_POST["ast_no_missions"])) {  
+              $ast_errors['ast_no_missions'] = 'An Input of into this field is required*';  
+          } else {  
+              $ast_no_missions = ($_POST["ast_no_missions"]);  
+                   //check if name only contains letters and whitespace  
+                   if (!preg_match("/^[a-zA-Z ]*$/",$ast_no_missions)) {  
+                      $ast_errors['ast_no_missions'] = 'An Input of date is required';
+               }
+             }
+                
+      } 
       ?>
         <!-- Class row and column used to create a space for main title of the page, justify content to move the content to the center,
         bg is used for background and text-white is to make the text white-->
