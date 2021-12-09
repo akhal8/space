4d1cb41da883d3a8846e3c27b2a65ca9b8f9349c
akhal8 <alikhalid1024@gmail.com>
2021-12-09 19:04:11 +0000

    update

--- ast_adding.php
+++ ast_adding.php
@@ -1,12 +1,13 @@
 <?php
 /* connecting this file to link file where the connection details between the database are saved*/
 include 'link.php';
+include 'validation 2.php';
 /*getting the details from the form and saving it into the variables*/
-$name = $_POST["name"];
-$no_missions = $_POST["no_missions"];
+$astronaut_name = $_POST["ast_name"];
+$astronaut_no_missions = $_POST["ast_no_missions"];
 
 /* creating a query to put the data that we saved in variables and put that in the database table astronaut in different coloumns*/
-$sql = "INSERT INTO astronaut(name, no_mission) VALUES ('$name', '$no_missions')";  
+$sql = "INSERT INTO astronaut(name, no_mission) VALUES ('$astronaut_name', '$astronaut_no_missions')";  
 /*Creating an if statement to check if query is successfull or */
 /*if the query is unsuccesfull show an error*/
 if(!mysqli_query($connection, $sql)){
