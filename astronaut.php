059938cc9490ab380aa1c4665bc1a31c4695ed95
akhal8 <alikhalid1024@gmail.com>
2021-12-09 19:12:17 +0000

    update

--- astronaut.php
+++ astronaut.php
@@ -46,9 +46,9 @@
         <div class="col-md-12 justify-content-center text-center p-2">
         <form action="ast_adding.php" method="POST">
         <label style="font-size: 12pt; color: blue;"><?php echo $errors['ast_name']; ?></label>
-        <div class= "p-1">Astronaut name</div> <input type="text" name="ast_name" placeholder="James"><br>
+        <div class= "p-1">Astronaut name</div> <input type="text" name="ast_name" placeholder="James" required><br>
         <label style="font-size: 12pt; color: blue;"><?php echo $errors['ast_no_missions']; ?></label>
-        <div class= "p-1">Number of Missions</div> <input type="text" name="ast_no_missions" placeholder="10,11,20..."><br>
+        <div class= "p-1">Number of Missions</div> <input type="text" name="ast_no_missions" placeholder="10,11,20..." required><br>
         <div class= "p-1"><input type="submit"></div>
         </form>
         <a div class="col-md-12 justify-content-center text-center p-2" href='index.php'><button>Main Page</button></a>
