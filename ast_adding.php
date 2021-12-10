9bde0ec4db4a0687515b9a94839caa3807acea3f
akhal8 <alikhalid1024@gmail.com>
2021-12-10 10:25:18 +0000

    update

--- ast_adding.php
+++ ast_adding.php
@@ -4,8 +4,8 @@ include 'link.php';
 
 /*getting the details from the form and saving it into the variables*/
 
-$astronaut_name = test_input($_POST["last_name"];
-$astronaut_no_missions = test_input($_POST["no_missions"];
+$astronaut_name = $_POST["last_name"];
+$astronaut_no_missions = $_POST["no_missions"];
 
 /* creating a query to put the data that we saved in variables and put that in the database table astronaut in different coloumns*/
 $sql = "INSERT INTO astronaut(name, no_mission) VALUES ('$astronaut_name', '$astronaut_no_missions')";  
