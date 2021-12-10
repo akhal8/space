1217b2fa0c955d3d14193741c814e9305ef58efd
akhal8 <alikhalid1024@gmail.com>
2021-12-10 10:22:01 +0000

    update

--- ast_adding.php
+++ ast_adding.php
@@ -1,7 +1,6 @@
 <?php
 /* connecting this file to link file where the connection details between the database are saved*/
 include 'link.php';
-include 'validation2.php';
 
 /*getting the details from the form and saving it into the variables*/
 
